baec4f69f56ff335d6bc9c2c0880f0a567120b23
llucas314 <llucas314@gmail.com>
2019-08-17 12:27:18 -0400

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,8 @@
+console.log("Up and running!");
+var cardOne = "queen";
+var cardTwo = "king";
+var cardThree = "king";
+var cardFour = "queen";
+
+console.log("User flipped " + cardFour);
+console.log("User flipped " + cardThree);
