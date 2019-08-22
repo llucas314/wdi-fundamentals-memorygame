18a38176ae67b7c75dff4c24d0f0bd2c42d1ff69
llucas314 <llucas314@gmail.com>
2019-08-22 08:08:00 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -28,14 +28,24 @@ function checkForMatch() {
 			alert("Sorry, try again!");
 		}
 }
-function flipCards(cardId) {
+function flipCard() {
+	var cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
 	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src', cards[cardId].cardImage);
 	if (cardsInPlay.length === 2){
 		checkForMatch();
 	}
 }
-flipCards(0);
-flipCards(2);
+function createBoard() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', "images/back.png");
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+createBoard();
