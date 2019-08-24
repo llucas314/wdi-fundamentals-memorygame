6c5ac04b279eadcb4cba882ce08b07b8e57ecb50
llucas314 <llucas314@gmail.com>
2019-08-24 01:26:20 -0400

    added more functionality

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,50 +2,163 @@ var cards = [
 {
 	rank: "queen",
 	suit: "hearts",
-	cardImage: "images/queen-of-hearts.png"
+	cardImage: "images/queen-of-hearts.png",
+	flipped: false,
+	matched: false,
+	cardNum: 0
 },
 {
 	rank: "queen",
 	suit: "diamonds",
-	cardImage: "images/queen-of-diamonds.png"
+	cardImage: "images/queen-of-diamonds.png",
+	flipped: false,
+	matched: false,
+	cardNum: 1
 },
 {
 	rank: "king",
 	suit: "hearts",
-	cardImage: "images/king-of-hearts.png"
+	cardImage: "images/king-of-hearts.png",
+	flipped: false,
+	matched: false,
+	cardNum: 2
 },
 {
 	rank: "king",
 	suit: "diamonds",
-	cardImage: "images/king-of-diamonds.png"
+	cardImage: "images/king-of-diamonds.png",
+	flipped: false,
+	matched: false,
+	cardNum: 3
+},
+{
+	rank: "joker",
+	suit: "joker",
+	cardImage: "images/joker.png",
+	flipped: false,
+	matched: false,
+	cardNum: 4
 }
 ];
 var cardsInPlay = [];
-function checkForMatch() {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-			alert("You found a match!");
-		} else {
-			alert("Sorry, try again!");
+var resultBox = document.createElement('p');
+var resultDiv = document.getElementById('result');
+var availableNum = [];
+var matches = 0;
+var correctMatch = ["You got it!", "Way to go!", "Yes! That's it!", "Awesome match!", "Watson! I think we have a match!", "If that's not a match, I don't know what is!"];
+var incorrectMatch = ["That's not it!", "No way, Jose!", "Umm, I don't think so!", "Use your memory!", "Houston, we have a problem!"];
+
+//reset last two cards
+function reset() {
+	for (var i = cardsInPlay.length - 1; i > cardsInPlay.length - 3; i--) {
+		var resetLastTwoCards = document.getElementsByTagName('img')[cardsInPlay[i].cardNum];
+		if (cardsInPlay[i].flipped === true && cardsInPlay[i].matched === false) {
+			resetLastTwoCards.setAttribute('src', 'images/back.png');
+			cardsInPlay[i].flipped = false;
 		}
+	}
 }
+function resetAll() {
+
+	for (var i = 0; i < cards.length; i++) {
+		var resetCards = document.getElementsByTagName('img')[i];
+		if (resetCards.getAttribute('src') !== 'images/back.png') {
+			resetCards.setAttribute('src', 'images/back.png');
+		}
+		cards[i].flipped = false;
+		cards[i].matched = false;
+		matches = 0;
+	}
+	resultBox.innerHTML = "Find a Match!";
+
+}
+
+function checkForMatch() {
+	var minusOne = cardsInPlay[cardsInPlay.length - 1];
+	var minusTwo = cardsInPlay[cardsInPlay.length - 2];
+
+	if (minusOne.rank !== minusTwo.rank) {
+		resultBox.innerHTML = incorrectMatch[Math.floor(Math.random()*incorrectMatch.length)];
+		setTimeout(reset, 1300);
+	} else if (minusOne.suit !== minusTwo.suit) {
+		resultBox.innerHTML = correctMatch[Math.floor(Math.random()*correctMatch.length)];
+		minusOne.matched = true;
+		minusTwo.matched = true;
+		matches++;
+		if (matches === 2) {
+			resultBox.innerHTML = "You matched all the cards!"
+			
+			cardsInPlay.length = 0;
+			setTimeout(playAgain, 3000);
+			//createBoard();
+		}
+	}
+}
+
 function flipCard() {
 	var cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
-	cardsInPlay.push(cards[cardId].rank);
+	if (cards[cardId].flipped === false) {
+		cardsInPlay.push(cards[cardId]);
+	}
+	cards[cardId].flipped = true;
 	this.setAttribute('src', cards[cardId].cardImage);
-	if (cardsInPlay.length === 2){
+	if (cardsInPlay.length % 2 === 0){
 		checkForMatch();
 	}
 }
+function randNum(){
+var num = Math.floor((Math.random()*5));
+while (availableNum.includes(num)){
+	num = Math.floor((Math.random()*5))
+}
+if (!availableNum.includes(num)) {
+	availableNum.push(num);
+	return num;
+}
+}
+function tryAgain(){
+	resultBox.innerHTML = "Find a Match!";
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.getElementsByTagName('img')[i];
+		cardElement.setAttribute('src', "images/back.png");
+		var randCardNum = randNum();
+		cards[randCardNum].cardNum = i;
+		cardElement.setAttribute('data-id', randCardNum);
+		console.log("card " + i + " = " + randCardNum);
+	}
+	
+}
+function removeBoard(){
+	var parent = document.getElementById('game-board');
+	var parent2 = document.getElementById('text');
+	var child2 = document.getElementById('result');
+	for (var i = 0; i < cards.length; i++) {
+		var child1 = document.getElementById('card' + i);
+		parent.removeChild(child1);
+	}
+	parent2.removeChild(child2);
+}
 function createBoard() {
+	resultBox.innerHTML = "Find a Match!";
+	resultBox.setAttribute('id', 'box');
+	resultDiv.appendChild(resultBox);
 	for (var i = 0; i < cards.length; i++) {
 		var cardElement = document.createElement('img');
+		cardElement.setAttribute('id', 'card' + i);
 		cardElement.setAttribute('src', "images/back.png");
-		cardElement.setAttribute('data-id', i);
+		var randCardNum = randNum();
+		cards[randCardNum].cardNum = i;
+		cardElement.setAttribute('data-id', randCardNum);
 		cardElement.addEventListener('click', flipCard);
 		document.getElementById('game-board').appendChild(cardElement);
+		console.log("card " + i + " = " + randCardNum);
 	}
+}
+function playAgain() {
+	resultBox.innerHTML = "<button class=\"button\" onClick=\"window.location.reload();\">Play Again</button>";
+	//document.getElementsByTagName('button')[0].addEventListener('click', tryAgain);
 }
 createBoard();
